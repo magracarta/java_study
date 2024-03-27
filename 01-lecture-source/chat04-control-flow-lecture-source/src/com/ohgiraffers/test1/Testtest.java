75887a9c0777fde62c2830ab3b357c67bdbbd42e
DESKTOP-R4CC0BA\MinJu <rmfoal1996@gmail.com>
2024-03-27 20:51:36 +0900

    no message

--- 01-lecture-source/chat04-control-flow-lecture-source/src/com/ohgiraffers/test1/Testtest.java
+++ 01-lecture-source/chat04-control-flow-lecture-source/src/com/ohgiraffers/test1/Testtest.java
@@ -1,16 +1,60 @@
 package com.ohgiraffers.test1;
 
+import java.sql.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Testtest {
     public static void main(String[] args) {
+
+        //깊은 복사
+
+        // 1. clone
+
+        int [] arr = {1,3,4,5};
+
+        //int [] copyArr = arr.clone();
+        //int[] copyArr = new int[8];
+        //System.arraycopy(arr, 2, copyArr,0,arr.length-2);
+
+        int[] copyArr = Arrays.copyOf(arr,8);
+
+
+        System.out.println(copyArr.hashCode());
+        System.out.println(arr.hashCode());
+
+
+
+        for (int x : arr){
+            System.out.print(x + " ");
+        }
+
+        System.out.println();
+
+        for (int x : copyArr){
+            System.out.print(x + " ");
+        }
+
+        System.out.println();
+        System.out.println("----------------------------------------------");
+        System.out.println("---------------  정렬 알고리즘  -----------");
+
+
         int [] ar = {89,56,21,87,55,44,33,85,31,12};
 
-        //순차 알고리즘
+        for (int x : ar){
+            System.out.print(x + " ");
+        }
+
+        System.out.println();
+
+
+
+        //1. 순차 알고리즘
         /*
-        for(int i =1; i< ar.length; i++){
-            for(int j =0; j < i; j++){
-                if(ar[j] > ar[i]){
+        for (int i = 1; i < ar.length; i++){
+            for(int j = 0;  j < i; j++){
+                if(ar[i] <  ar[j]){
                     int temp = ar[i];
                     ar[i] = ar[j];
                     ar[j] = temp;
@@ -19,49 +63,39 @@ public class Testtest {
         }
 
          */
-
-        //선택정렬
+        
+        //2. 선택 알고리즘
         /*
         int min;
         int temp;
-
-        for(int i =0; i < ar.length-1; i++){
+        for(int i = 0; i < ar.length-1; i++){
             min = i;
-            for(int j = i+1; j < ar.length; j++){
+            for(int j =i+1; j < ar.length; j++){
                 if(ar[min] > ar[j]){
                     min = j;
                 }
             }
-            temp = ar[min];
-            ar[min] = ar[i];
-            ar[i] =temp;
-        }
-
-         */
-
-       // 삽입 정렬
-        /*
-        int temp;
-        int j =0;
-
-        for(int i =1; i <ar.length; i++ ){
             temp = ar[i];
-            for(j = i-1; j >=0 && ar[j] > temp ; j--){
-                ar[j+1] = ar[j];
+            ar[i] = ar[min];
+            ar[min] =  temp;
+            for (int x : ar){
+                System.out.print(x + " ");
             }
-            ar[j+1] = temp;
+
+            System.out.println();
         }
+
 
          */
 
-        //버블 정렬
+        //3. 버블 정렬 알고리즘
         /*
-        for(int i = ar.length-1; i>0; i--){
+        for(int i = ar.length-1; i >=0; i-- ){
             for(int j = 0; j < i; j++){
-                if(ar[j] > ar[j+1] ){
+                if(ar[j] > ar[j+1]){
                     int temp = ar[j];
                     ar[j] = ar[j+1];
-                    ar[j+1] = temp;
+                    ar[j+1]= temp;
                 }
             }
 
@@ -70,7 +104,19 @@ public class Testtest {
          */
 
 
-        // i 가 0인 정렬 : 선택
+        //4. 삽입 정렬 알고리즘
+        int j;
+        int temp;
+
+        for(int  i = 1; i < ar.length; i++){
+            temp = ar[i];
+            for(j = i-1; j >=0 && ar[j] > temp; j-- ){
+                ar[j+1] = ar[j];
+            }
+            ar[j+1] = temp;
+
+        }
+
 
 
         for (int x : ar){
