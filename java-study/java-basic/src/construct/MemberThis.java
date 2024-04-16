a857a971545911565e465925214034df95e07f53
magracarta <rmfoal1996@gmail.com>
2024-04-16 13:55:19 +0900

    no message

---
+++ java-study/java-basic/src/construct/MemberThis.java
@@ -0,0 +1,9 @@
+package construct;
+
+public class MemberThis {
+    String nameFiled;
+
+    void initMember(String namePrarmeter) {
+        nameFiled = namePrarmeter; //this 생략가능
+    }
+}
--- java-study/java-basic/src/construct/Memberinit.java
+++ java-study/java-basic/src/construct/Memberinit.java
@@ -4,6 +4,7 @@ public class Memberinit {
     String name;
     int age;
     int grade;
+    //추가
     void initMember( String neme , int age , int grade ){
         this.name = neme;
         this.age = age;
