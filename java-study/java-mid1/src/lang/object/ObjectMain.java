40a36fad71bf8a904b187391a85573b150cb3886
magracarta <rmfoal1996@gmail.com>
2024-04-30 12:00:42 +0900

    no message

--- java-study/java-mid1/src/lang/object/ObjectMain.java
+++ java-study/java-mid1/src/lang/object/ObjectMain.java
@@ -5,7 +5,7 @@ public class ObjectMain {
         Child child = new Child();
         child.childMethod();
         child.parentMethod();
-        
+
 
         //string의 Object 클래스의 메서드이다.
         String string = child.toString();
