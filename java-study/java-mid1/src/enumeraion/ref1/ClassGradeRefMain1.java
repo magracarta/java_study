cc586453cba766bae73de9f6b1154d39cd5a0766
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2024-06-04 21:24:04 +0900

    no message

--- java-study/java-mid1/src/enumeraion/ref1/ClassGradeRefMain1.java
+++ java-study/java-mid1/src/enumeraion/ref1/ClassGradeRefMain1.java
@@ -11,6 +11,6 @@ public class ClassGradeRefMain1 {
 
         System.out.println("BASIC 등급의 할인 금액 : "+ basic);
         System.out.println("GOLD 등급의 할인 금액 : "+ gold);
-        System.out.println("DIAMONDD 등급의 할인 금액 : "+ diamond);
+        System.out.println("DIAMOND 등급의 할인 금액 : "+ diamond);
     }
 }
