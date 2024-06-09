a132ca1b04c0b2c45015a79c2ccbda9371101d4e
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2024-06-09 21:00:26 +0900

    no message

---
+++ java-study/java-mid1/src/time/FormattingMain1.java
@@ -0,0 +1,26 @@
+package time;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class FormattingMain1 {
+    public static void main(String[] args) {
+        // 포멧팅 : 날짜를 문자로
+        LocalDate date = LocalDate.of(2024,12,31);
+        System.out.println("date = " + date);
+        System.out.println(date.getYear() + "년 " + date.getMonthValue() + " 월");
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일");
+        String foramteDate = date.format(formatter);
+        System.out.println("날짜와 시간 포멧팅"+foramteDate);
+
+
+        //파싱 : 문자를 날짜로
+        String input = "2030년 01월 01일";
+        LocalDate parseDate = LocalDate.parse(input , formatter);
+
+        System.out.println(parseDate);
+
+
+    }
+}
