f741a47a06bc9d20d76d04c2479ade9b3e8ee939
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2024-06-19 23:44:52 +0900

    no message

--- java-study/java-mid1/src/exeption/ex1/MainV1.java
+++ java-study/java-mid1/src/exeption/ex1/MainV1.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 
 public class MainV1 {
     public static void main(String[] args) {
-        NetworkServiceV0_1 networkSerivce = new NetworkServiceV0_1();
+        //NetworkServiceV0_1 networkSerivce = new NetworkServiceV0_1();
+        NetworkServiceV0_2 networkSerivce = new NetworkServiceV0_2();
+        //NetworkServiceV0_3 networkSerivce = new NetworkServiceV0_3();
         Scanner sc = new Scanner(System.in);
         while(true){
             System.out.print("전송할 문자 : ");
