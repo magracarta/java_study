f9afbcdcbb557533322df5aec856a7f00fe49b04
magracarta <rmfoal1996@gmail.com>
2024-06-28 14:01:38 +0900

    no message

--- java-study/java-mid1/src/exeption/ex4/MainV4.java
+++ java-study/java-mid1/src/exeption/ex4/MainV4.java
@@ -9,7 +9,8 @@ public class MainV4 {
     public static void main(String[] args){
 
         //NetworkServiceV3_1 networkSerivce = new NetworkServiceV3_1();
-        NetworkServiceV4 networkSerivce = new NetworkServiceV4();
+        //NetworkServiceV4 networkSerivce = new NetworkServiceV4();
+        NetworkServiceV5 networkSerivce = new NetworkServiceV5();
 
         Scanner sc = new Scanner(System.in);
         while(true){
@@ -21,8 +22,9 @@ public class MainV4 {
             try {
                 networkSerivce.sendMessage(input);
             }catch (Exception e){
+                //e.printStackTrace();
                 exceptionHandelr(e);
-            }
+             }
             System.out.println();
         }
 
@@ -35,8 +37,8 @@ public class MainV4 {
         //공통 처리
         System.out.println("사용자 메세지 : 죄송합니다. 알수없는 문제가 발생했습니다.");
         System.out.println("-- 개발자용 디버깅 메시지 --");
-        //e.printStackTrace(System.out);//스택트레이스
-        e.printStackTrace();
+        e.printStackTrace(System.out);//스택트레이스
+        //e.printStackTrace();
 
         //필요하면 예외 별로 별도의 추가 처리 가능
         if(e instanceof SendExceptionV4 sendEx){
