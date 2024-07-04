237d533065c7c4f60edf03fec5af0a37352eed9f
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2024-07-04 21:46:22 +0900

    no message

--- java-study/java-mid2/src/generic/test/ex5/WildcardEx.java
+++ java-study/java-mid2/src/generic/test/ex5/WildcardEx.java
@@ -7,14 +7,28 @@ public class WildcardEx {
 
         System.out.println("T = " + box.get());
     }
+
+    static void  printWildCardV1(Box<?> box){
+        System.out.println("? = " + box.get());
+    }
+
     static <T extends Aniamll> void printGenericV2(Box<T> box){
         T t = box.get();
         System.out.println("이름 = " + t.getName());
+    }
+    static void printWildCardV2(Box<? extends  Aniamll> box){
+        Aniamll animal = box.get();
+        System.out.println("이름 = " + animal.getName());
     }
 
     static <T extends Aniamll> T printAndReturnGenericV3(Box<T> box){
         T t = box.get();
         System.out.println("이름 = " + t.getName());
         return  t;
+    }
+    static Aniamll printAndReturnWildV3(Box<? extends  Aniamll> box){
+       Aniamll aniaml = box.get();
+        System.out.println("이름 = " + aniaml.getName());
+        return  aniaml;
     }
 }
--- java-study/java-mid2/src/generic/test/ex5/WildcardMain1.java
+++ java-study/java-mid2/src/generic/test/ex5/WildcardMain1.java
@@ -1,4 +1,27 @@
 package generic.test.ex5;
 
+import generic.animall.Aniamll;
+import generic.animall.Cat;
+import generic.animall.Dog;
+
 public class WildcardMain1 {
+    public static void main(String[] args) {
+        Box<Object> objectBox = new Box<>();
+        Box<Dog> dogBox = new Box<>();
+        Box<Cat> catBox = new Box<>();
+
+
+        dogBox.set(new Dog("멍멍이", 100));
+        catBox.set(new Cat("고양이", 100));
+
+        WildcardEx.printGenericV1(dogBox);
+        WildcardEx.printGenericV2(dogBox);
+
+        WildcardEx.printWildCardV1(dogBox);
+        WildcardEx.printWildCardV2(dogBox);
+
+        Aniamll animal = WildcardEx.printAndReturnWildV3(dogBox);
+
+        Cat cat = WildcardEx.<Cat>printAndReturnGenericV3(catBox);
+    }
 }
