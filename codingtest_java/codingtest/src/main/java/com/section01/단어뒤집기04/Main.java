deab42785dbfcb308b9104cb991fc38efa7d02de
magracarta <rmfoal1996@gmail.com>
2024-12-25 16:44:40 +0900

    단어 뒤집기

---
+++ codingtest_java/codingtest/src/main/java/com/section01/단어뒤집기04/Main.java
@@ -0,0 +1,38 @@
+package com.section01.단어뒤집기04;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public String[] solution(int n , String [] strs){
+        String [] answer = new String[n];
+
+        for (int i = 0; i < n; i++){
+            /*String reverseText = "";
+            for(int k =strs[i].length()-1; k >= 0; k--){
+                reverseText += strs[i].charAt(k);
+            }*/
+            answer[i] = new StringBuilder(strs[i]).reverse().toString();
+        }
+
+        return  answer;
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        String [] str = new String[n];
+
+        for (int i = 0; i < n; i++) str[i] = sc.nextLine();
+
+        String[] answer = main.solution(n, str);
+        for (int i = 0; i < answer.length; i++) System.out.println(answer[i]);
+
+
+
+    }
+
+
+}
