dcd221c52ebbb39f01c1acb9f2e9a928d0535073
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2026-08-02 14:48:11 +0900

    날짜 끝

---
+++ java-study/java-mid1/src/time/test/TestCalenderPrinter.java
@@ -0,0 +1,44 @@
+package time.test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Scanner;
+
+public class TestCalenderPrinter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("년도를 입력하세요.");
+        int year = scanner.nextInt();
+        System.out.println("월을 입력하세요.");
+        int month = scanner.nextInt();
+
+        printCalander (year, month);
+
+    }
+
+    private static void printCalander(int year, int month) {
+        LocalDate firstDayOfMonth = LocalDate.of(year, month, 1);
+        LocalDate firstDayOfNextMonth = firstDayOfMonth.plusMonths(1);
+
+        //월요일 = 1 ... 일요일7
+        int offsetWeekDays = firstDayOfMonth.getDayOfWeek().getValue() % 7;
+        System.out.println("Su Mo Tu We Th Fr Sa");
+
+        for(int i =0; i < offsetWeekDays; i++) {
+            System.out.print("   ");
+        }
+
+        LocalDate dayIterator = firstDayOfMonth;
+        while (dayIterator.isBefore(firstDayOfNextMonth)) {
+            System.out.printf("%2d ",dayIterator.getDayOfMonth());
+            if(dayIterator.getDayOfWeek() == DayOfWeek.SATURDAY) {
+                System.out.println();
+            }
+            dayIterator = dayIterator.plusDays(1);
+        }
+
+
+    }
+}
