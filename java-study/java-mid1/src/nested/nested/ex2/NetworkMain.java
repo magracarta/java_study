b122d434272ca678de1ed891cecf7ed4ebe9b6b5
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2026-08-04 20:52:06 +0900

    정적중첩클래스

---
+++ java-study/java-mid1/src/nested/nested/ex2/NetworkMain.java
@@ -0,0 +1,8 @@
+package nested.nested.ex2;
+
+public class NetworkMain {
+    public static void main(String[] args) {
+        NetWork netWork = new NetWork();
+        netWork.sendMessage("hello java");
+    }
+}
