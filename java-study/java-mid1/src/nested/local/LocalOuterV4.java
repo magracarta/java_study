e81ef444c5158cc1774c56f850eb9d5a07de3240
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2026-08-11 22:42:28 +0900

    익명클래스

--- java-study/java-mid1/src/nested/local/LocalOuterV4.java
+++ java-study/java-mid1/src/nested/local/LocalOuterV4.java
@@ -2,7 +2,7 @@ package nested.local;
 
 import java.lang.reflect.Field;
 
-public class LocalOuterV3 {
+public class LocalOuterV4 {
     private int outInstanceVar =3;
     public Printer process(int paramVar){
         int localVar = 1; // 지역변수는 스택 프레임이 종료되는 순간 함께 제거된다.
@@ -21,12 +21,14 @@ public class LocalOuterV3 {
         }
 
         LocalPrinter printer = new LocalPrinter();
-//        printer.print(); 를 실행하지 않고 반환만 한다.
+        //만약 localVar 의 값을 변경한다면? 다시 캡쳐해야하나?
+        //localVar = 10;
+        //paramVar = 20;
         return printer;
     }
 
     public static void main(String[] args) {
-        LocalOuterV3 localOuter = new LocalOuterV3();
+        LocalOuterV4 localOuter = new LocalOuterV4();
         Printer printer = localOuter.process(2);
         //printer.print()를 나중에 실행한다.  process()의 스택 프레임이 사라진 이후에 실행
         printer.print();
