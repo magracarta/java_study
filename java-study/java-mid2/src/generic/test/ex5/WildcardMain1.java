6c5f808b94c64fa9c9796904461dfd585e5cfba8
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2026-09-21 21:09:18 +0900

    와일드카드

---
+++ java-study/java-mid2/src/generic/test/ex5/WildcardMain1.java
@@ -0,0 +1,23 @@
+package generic.test.ex5;
+
+import generic.animal.Animal;
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class WildcardMain1 {
+    public static void main(String[] args) {
+        Box<Object> objBox = new Box<>();
+        Box<Dog> dogBox = new Box<>();
+        Box<Cat> catBox = new Box<>();
+
+        dogBox.set(new Dog("멍멍이", 100));
+        WildCardEx.<Dog>printGenericV1(dogBox);
+        WildCardEx.printGenericV2(dogBox);
+        Dog dog = WildCardEx.printAndReturn(dogBox);
+
+        WildCardEx.printWildCardV1(dogBox);
+        WildCardEx.printWildCardV2(catBox);
+        Animal animal = WildCardEx.printAndReturnWildCard(dogBox);
+        System.out.println("애니몰"+animal);
+    }
+}
